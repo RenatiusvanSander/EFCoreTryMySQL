f0c09f60fa0cb094f69358f9870978a68bf95ac2
Worker <remad@web.de>
2019-05-14 00:58:33 +0200

    all files are commited

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using EFCoreTry.EFModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Text;
+
+namespace EFCoreTry
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            InsertData();
+            PrintData();
+        }
+
+        private static void InsertData()
+        {
+            using (var context = new LibraryContext())
+            {
+                // Creates the database if not exists
+                context.Database.EnsureCreated();
+
+                // Adds a publisher
+                var publisher = new Publisher
+                {
+                    Name = "Mariner Books"
+                };
+                context.Publisher.Attach(publisher);
+
+                // Adds some books
+                context.Book.Attach(new Book
+                {
+                    ISBN = "978-0544003415",
+                    Title = "The Lord of the Rings",
+                    Author = "J.R.R. Tolkien",
+                    Language = "English",
+                    Pages = 1216,
+                    Publisher = publisher
+                });
+                context.Book.Attach(new Book
+                {
+                    ISBN = "978-0547247762",
+                    Title = "The Sealed Letter",
+                    Author = "Emma Donoghue",
+                    Language = "English",
+                    Pages = 416,
+                    Publisher = publisher
+                });
+
+                // Saves changes
+                context.SaveChanges();
+            }
+        }
+
+        private static void PrintData()
+        {
+            // Gets and prints all books in database
+            using (var context = new LibraryContext())
+            {
+                var books = context.Book
+                  .Include(p => p.Publisher);
+                foreach (var book in books)
+                {
+                    var data = new StringBuilder();
+                    data.AppendLine($"ISBN: {book.ISBN}");
+                    data.AppendLine($"Title: {book.Title}");
+                    data.AppendLine($"Publisher: {book.Publisher.Name}");
+                    Console.WriteLine(data.ToString());
+                }
+            }
+
+            // Pause to hold programm.
+            Console.ReadKey();
+        }
+    }
+}
